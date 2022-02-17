eeec373958e4768d2fd954f30c88589e620fa8ed
Daniel Milan <danielmilanvera@gmail.com>
2022-02-17 19:42:20 +0100

    rooms.js  line 57 not working

--- IronRooms/routes/rooms.js
+++ IronRooms/routes/rooms.js
@@ -3,6 +3,16 @@ const Room = require("../models/Room.model")
 const isLoggedIn = require("../middleware/isLoggedIn")
 
 
+router.post("/:id/delete", (req, res)=>{
+    const id = req.params.id
+
+    Room.findByIdAndDelete(id)
+        .then((room)=>{
+            res.redirect("/rooms")
+            
+        })
+
+})
 
 router
     .route("/:id/edit")
@@ -25,30 +35,41 @@ router
             })
     })
 
-
-
-router
+    
+    router
     .route("/create")
     .get(isLoggedIn,(req, res) => {
         res.render("rooms/new-room")
     })
     .post((req, res) => {
-
+        
         const {name, description, imageUrl} = req.body
         const owner = req.session.user
-
+        
         Room.create({name, description, imageUrl, owner})
-            .then((newRoom) => {
-                console.log(newRoom, owner)
-                res.redirect("/rooms")
-            })
-         })
+        .then((newRoom) => {
+            console.log(newRoom, owner)
+            res.redirect("/rooms")
+        })
+    })
 
-router.get("/", (req, res) => {
-    Room.find()
+
+    router.get("/:id/details",isLoggedIn,(req, res) => {
+        const roomId = req.params.id
+
+        Room.findById(roomId)
+            .then((room)=>{
+                res.render("room/room", room)
+            })
+    })
+
+
+    
+    router.get("/", (req, res) => {
+        Room.find()
         .then((roomsList)=>{
             res.render("rooms/roomsList", {roomsList})
         })
-})
-
-module.exports = router;
+    })
+    
+    module.exports = router;
